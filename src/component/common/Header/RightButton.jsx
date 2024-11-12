4af489da2dc61d921fa49d0fe563f8772cde6720
jongbin26 <010jjj@naver.com>
2024-11-12 13:35:04 +0900

    feat: Header 컴포넌트 구현

---
+++ src/component/common/Header/RightButton.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useTheme } from 'styled-components';
+import styled from 'styled-components';
+
+const RightButton = () => {
+  const theme = useTheme();
+  const navigate = useNavigate();
+
+  //isLoggedIn 추후에 변경
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  const NoneAuthButtonComponent = () => {
+    return (
+      <S.ButtonWrapper>
+        <button onClick={() => navigate('./login')}>
+          <p style={{ fontSize: 16, color: theme.colors.grey3 }}>로그인</p>
+        </button>
+        <div style={{ width: 1, height: 12, background: theme.colors.grey3 }} />
+        <button onClick={() => navigate('./signup')}>
+          <p style={{ fontSize: 16, color: theme.colors.grey3 }}>회원가입</p>
+        </button>
+      </S.ButtonWrapper>
+    );
+  };
+
+  const AuthButtonComponent = () => {
+    return (
+      <S.ButtonWrapper>
+        <button onClick={() => navigate('./mypage')}>
+          <p style={{ fontSize: 16, color: theme.colors.grey3 }}>마이페이지</p>
+        </button>
+      </S.ButtonWrapper>
+    );
+  };
+
+  return isLoggedIn ? <AuthButtonComponent /> : <NoneAuthButtonComponent />;
+};
+
+const S = {
+  ButtonWrapper: styled.div`
+    display: flex;
+    gap: 15px;
+    align-items: center;
+  `,
+};
+
+export default RightButton;
