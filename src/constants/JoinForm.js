16fec10479353bff125cbeefb84951f29b19e33b
goeun kim <rlarhdms6828@naver.com>
2024-11-17 20:19:47 +0900

    Feat: 회원가입 입력폼 상수처리

---
+++ src/constants/JoinForm.js
@@ -0,0 +1,58 @@
+const JOIN_FORM = [
+  {
+    label: '이메일',
+    info: [{ type: 'text', placeholder: '이메일을 입력하세요', name: 'email' }],
+  },
+  {
+    label: '비밀번호',
+    info: [
+      {
+        type: 'password',
+        placeholder: '비밀번호를 입력하세요',
+        name: 'password',
+      },
+    ],
+  },
+  {
+    label: '비밀번호 확인',
+    info: [
+      {
+        type: 'password',
+        placeholder: '비밀번호를 입력하세요',
+        name: 'confirmPwd',
+      },
+    ],
+  },
+  {
+    label: '이름',
+    info: [
+      {
+        type: 'text',
+        placeholder: '이름을 입력하세요',
+        name: 'name',
+      },
+    ],
+  },
+  {
+    label: '학번',
+    info: [
+      {
+        type: 'number',
+        placeholder: '학번을 입력하세요',
+        name: 'studentNum',
+      },
+    ],
+  },
+  {
+    label: '전회번호',
+    info: [
+      {
+        type: 'text',
+        placeholder: '전화번호를 입력하세요',
+        name: 'phoneNum',
+      },
+    ],
+  },
+];
+
+export default JOIN_FORM;
