c86b394f9fbac9156e0692bc77f3790404984466
goeun kim <rlarhdms6828@naver.com>
2024-11-19 19:27:45 +0900

    Feat: useNavigation 연결

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,8 +1,10 @@
 import UserPic from '../../assets/icon/UserPic.svg';
 import Vector2 from '../../assets/icon/Vector2.svg';
+import { useNavigate } from 'react-router-dom';
 import S from './style';
 
 const MyPage = () => {
+  const navigate = useNavigate();
   return (
     <S.Wrapper>
       <S.TopContainer>
@@ -13,17 +15,17 @@ const MyPage = () => {
         <img src={UserPic}></img>
       </S.TopContainer>
       <S.BottomContainer>
-        <S.Button>
+        <S.Button onClick={() => navigate('./edit')}>
           <S.ButtonIcon>✍🏻</S.ButtonIcon>
           <p>개인정보 수정</p>
           <S.ArrowIcon src={Vector2} />
         </S.Button>
-        <S.Button>
+        <S.Button onClick={() => navigate('./bookmark')}>
           <S.ButtonIcon>📑</S.ButtonIcon>
           <p>찜 목록</p>
           <S.ArrowIcon src={Vector2} />
         </S.Button>
-        <S.Button>
+        <S.Button onClick={() => navigate('./timetable')}>
           <S.ButtonIcon>📘</S.ButtonIcon>
           <p>내 시간표 관리</p>
           <S.ArrowIcon src={Vector2} />
