e1361263a50e7137cf4fc1441c25913e786c5509
goeun kim <rlarhdms6828@naver.com>
2024-11-19 19:24:13 +0900

    Design: 마이페이지 퍼블리싱

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,4 +1,35 @@
+import UserPic from '../../assets/icon/UserPic.svg';
+import Vector2 from '../../assets/icon/Vector2.svg';
+import S from './style';
+
 const MyPage = () => {
-  return <div>MyPage</div>;
+  return (
+    <S.Wrapper>
+      <S.TopContainer>
+        <div>
+          <h2>안녕하세요</h2>
+          <h1>시대평님</h1>
+        </div>
+        <img src={UserPic}></img>
+      </S.TopContainer>
+      <S.BottomContainer>
+        <S.Button>
+          <S.ButtonIcon>✍🏻</S.ButtonIcon>
+          <p>개인정보 수정</p>
+          <S.ArrowIcon src={Vector2} />
+        </S.Button>
+        <S.Button>
+          <S.ButtonIcon>📑</S.ButtonIcon>
+          <p>찜 목록</p>
+          <S.ArrowIcon src={Vector2} />
+        </S.Button>
+        <S.Button>
+          <S.ButtonIcon>📘</S.ButtonIcon>
+          <p>내 시간표 관리</p>
+          <S.ArrowIcon src={Vector2} />
+        </S.Button>
+      </S.BottomContainer>
+    </S.Wrapper>
+  );
 };
 export default MyPage;
