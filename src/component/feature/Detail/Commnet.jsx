f7d2de6576c634256f895903e5f43fd7f98f97d3
goeun kim <rlarhdms6828@naver.com>
2024-12-01 01:22:26 +0900

    Design/Feat: 댓글 영역 구현 및 별점 util 생성

--- src/component/feature/Detail/Commnet.jsx
+++ src/component/feature/Detail/Commnet.jsx
@@ -1,37 +1,47 @@
 import styled from 'styled-components';
 import { COMMENT } from '../../../constants/mocks/detailComment';
-import profile from '../../../assets/images/profile.svg';
-import rate4 from '../../../assets/icon/rating4.svg';
+import profile from '@assets/images/profile.svg';
+import rate4 from '@assets/icon/rating4.svg';
+import useDropdown from '@hook/UI/useDropdown';
+import { ratingUtil } from '@utils';
 
 const Comment = () => {
   const count = COMMENT.length;
+  const dropdown = useDropdown({
+    title: '평점별 모아보기',
+    items: ['평점 5점', '평점 4점', '평점 3점', '평점 2점', '평점 1점'],
+  });
   return (
     <S.Wrapper>
-      <div>
-        <h1>{`${count}개의 후기`}</h1>
-      </div>
-      <div>
+      <S.Header>
+        <div>
+          <S.Title>{`${count}개의 후기`}</S.Title>
+          {dropdown.render()}
+        </div>
+
+        <S.CommmentBtn>후기 남기기</S.CommmentBtn>
+      </S.Header>
+      <S.CommentsWrapper>
         {COMMENT.map((e) => (
           <S.CommentWrapper key={e.id}>
             <img src={profile} />
             <div>
-              <div>
+              <S.CommentBox>
                 <span>{e.name}</span>
                 <span>{e.date}</span>
-              </div>
-              <div>
-                <span>
-                  <label>{e.work}</label>
-                  <label>{e.mood}</label>
-                </span>
-                <img src={rate4} />
-              </div>
-              <div>{e.content}</div>
+              </S.CommentBox>
+              <S.CommentBox>
+                <S.ChipContainer>
+                  <S.Chip>{e.work}</S.Chip>
+                  <S.Chip>{e.mood}</S.Chip>
+                </S.ChipContainer>
+                <img src={ratingUtil(e.rate)} alt="rate" />
+              </S.CommentBox>
+              <S.CommentDetail>{e.content}</S.CommentDetail>
             </div>
           </S.CommentWrapper>
         ))}
-      </div>
-      <div></div>
+      </S.CommentsWrapper>
     </S.Wrapper>
   );
 };
@@ -45,8 +55,78 @@ const S = {
     flex-direction: column;
   `,
 
+  Header: styled.header`
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 1.5rem;
+    align-items: center;
+  `,
+
+  Title: styled.h1`
+    color: ${({ theme }) => theme.colors.black};
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 400;
+    margin-bottom: 10px;
+  `,
+
+  ChipContainer: styled.span`
+    display: flex;
+    align-items: center;
+    gap: 5px;
+  `,
+
+  Chip: styled.label`
+    border-radius: 20px;
+    padding: 3px 15px;
+    background-color: ${({ theme }) => theme.colors.blue};
+    color: ${({ theme }) => theme.colors.white};
+    font-family: Inter;
+    font-size: 11px;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: 0.5px;
+    text-transform: uppercase;
+  `,
+
+  CommmentBtn: styled.button`
+    color: ${({ theme }) => theme.colors.white};
+    border-radius: 20px;
+    background: ${({ theme }) => theme.colors.blue};
+
+    padding: 5px 25px;
+    line-height: 25px;
+
+    text-align: center;
+    font-size: 20px;
+    font-weight: 400;
+  `,
+  CommentsWrapper: styled.section`
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+
+    & div {
+      width: 100%;
+    }
+  `,
+
   CommentWrapper: styled.div`
     display: flex;
     width: 100%;
+    padding-bottom: 1rem;
+    margin-bottom: 1rem;
+    border-bottom: 1px solid #000;
+    gap: 10px;
+  `,
+
+  CommentBox: styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+  `,
+
+  CommentDetail: styled.div`
+    padding-top: 11px;
   `,
 };
--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,7 @@ export default defineConfig({
       '@assets': path.resolve(__dirname, './src/assets'),
       '@styles': path.resolve(__dirname, './src/styles'),
       '@component': path.resolve(__dirname, './src/component'),
+      '@utils': path.resolve(__dirname, './src/utils'),
     },
   },
 });
