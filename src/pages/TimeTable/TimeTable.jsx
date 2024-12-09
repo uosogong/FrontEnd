35055c67d491a15f0df9df89a239968709abd862
jongbin26 <010jjj@naver.com>
2024-12-09 21:41:45 +0900

    feat: 지원서 제출 API 부착

--- src/pages/TimeTable/TimeTable.jsx
+++ src/pages/TimeTable/TimeTable.jsx
@@ -3,8 +3,23 @@ import { useTableDragSelect } from 'use-table-drag-select';
 import Table from './Table.jsx';
 import { timeTableParsor } from '../../utils/timeTableParsor.js';
 import Button from '../../component/common/Button/Button.jsx';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { postFetcher } from '../../api/method.js';
+import useModal from '../../hook/UI/useModal.jsx';
 
 const TimeTable = () => {
+  const navigate = useNavigate();
+  const theme = useTheme();
+  const modal = useModal({
+    title: '저장되었습니다.',
+    onOk: () => navigate('/'),
+  });
+  const errorModal = useModal({
+    title: '네트워크 오류로 저장에 실패했습니다.',
+    onClick: () => {},
+  });
+
+  const location = useLocation();
   const [ref, value] = useTableDragSelect([
     [false, false, false, false, false, false, false],
     [false, false, false, false, false, false, false],
@@ -19,21 +34,48 @@ const TimeTable = () => {
   ]);
 
   const getJson = () => timeTableParsor(value);
-  console.log(getJson());
-  const theme = useTheme();
+
+  const submit = async () => {
+    try {
+      await postFetcher(`/resumes/${location.state.id}`, {
+        isScholarshipResume: location.state.applyForm.isScholarshipResume,
+        isInternResume: location.state.applyForm.isInternResume,
+        phone: location.state.applyForm.phone,
+        address: location.state.applyForm.address,
+        workplace: location.state.applyForm.workplace,
+        totalWorkSemester: location.state.applyForm.totalWorkSemester,
+        otherScholarship: location.state.applyForm.otherScholarship,
+        email: location.state.applyForm.email,
+        schedule: getJson(),
+        content: location.state.applyForm.content,
+        birthday: location.state.uneditableData.birthDay,
+      });
+      modal.open();
+    } catch (error) {
+      console.log(error);
+      errorModal.render();
+    }
+  };
+
   return (
-    <S.Wrapper>
-      <p style={{ fontSize: 28, fontWeight: 700, marginBottom: 40 }}>
-        시간표 작성 페이지
-      </p>
-      <Table ref={ref} value={value} />
-      <div style={{ display: 'flex', gap: 10 }}>
-        <Button style={{ width: 250 }}>제출</Button>
-        <Button style={{ width: 250, backgroundColor: theme.colors.grey2 }}>
-          임시저장
-        </Button>
-      </div>
-    </S.Wrapper>
+    <>
+      {modal.render()}
+      {errorModal.render()}
+      <S.Wrapper>
+        <p style={{ fontSize: 28, fontWeight: 700, marginBottom: 40 }}>
+          시간표 작성 페이지
+        </p>
+        <Table ref={ref} value={value} />
+        <div style={{ display: 'flex', gap: 10 }}>
+          <Button style={{ width: 250 }} onClick={submit}>
+            제출
+          </Button>
+          <Button style={{ width: 250, backgroundColor: theme.colors.grey2 }}>
+            임시저장
+          </Button>
+        </div>
+      </S.Wrapper>
+    </>
   );
 };
 export default TimeTable;
