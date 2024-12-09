e8b39cdc73c1300026d4e0e686a5be446678c512
goeun kim <rlarhdms6828@naver.com>
2024-12-10 01:02:53 +0900

    Feat: 마이페이지 지원목록

--- src/pages/Applicant/Applicant.jsx
+++ src/pages/Applicant/Applicant.jsx
@@ -2,27 +2,13 @@ import { S } from './style';
 import ApplicantItem from '../../component/feature/Applicant/ApplicantItem';
 import { getFetcher } from '../../api/method';
 import { useEffect, useState } from 'react';
-const data = {
-  id: 1,
-  tag: '직장체험인턴',
-  name: '조종빈',
-  studentId: 2020920059,
-  department: '컴퓨터과학부',
-  phoneNumber: '010-6374-8063',
-  //TODO
-  birth: '2001.02.26',
-  address: '망우로 18다길 19',
-  prevWorkPlace: '전산정보원',
-  totalWorkDuration: 3,
-  scholarship: '무슨무슨특별장학',
-  email: '010jjj@naver.com',
-  motivation:
-    '이런이런이유로 지원했습니다. 잘 봐주세요. 이런이런이유로 지원했습니다. 잘 봐주세요. 이런이런이유로 지원했습니다. 잘 봐주세요',
-};
+
 const Applicant = () => {
   const [resumes, setResumes] = useState([]);
   useEffect(() => {
-    fetchResume();
+    const { role } = JSON.parse(localStorage.getItem('userInfo'));
+    if (role == 'ADMIN') fetchResume();
+    else fetchMyResume();
   }, []);
 
   console.log(resumes);
@@ -31,6 +17,11 @@ const Applicant = () => {
     const res = await getFetcher(
       `/resumes?isScholarshipResume=true&isInternResume=true`,
     );
+    setResumes(res.message);
+  };
+
+  const fetchMyResume = async () => {
+    const res = await getFetcher(`/resumes/me`);
     setResumes(res.message);
   };
   return (
