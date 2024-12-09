ae2266ab2e8ba18e5af1398ecd0e58a02db4bfae
jongbin26 <010jjj@naver.com>
2024-12-09 19:51:50 +0900

    fix: StaffPage 기획 수정사항 반영

--- src/pages/Staff/Staff.jsx
+++ src/pages/Staff/Staff.jsx
@@ -2,46 +2,38 @@ import { useEffect, useRef, useState } from 'react';
 import Button from '../../component/common/Button/Button';
 import styled from 'styled-components';
 import { useTheme } from 'styled-components';
-import Input from '../../component/common/Input/Input';
 import TextArea from '../../component/common/TextArea';
+import DateInput from '../../component/common/DateInput /DateInput';
 import useModal from '../../hook/UI/useModal';
 
 const Staff = () => {
   const theme = useTheme();
-  const [category, setCategory] = useState(
-    localStorage.getItem('category') ?? '직장체험형 인턴',
-  );
-  const [isRecruiting, setIsRecruiting] = useState(
-    localStorage.getItem('isRecruiting') ?? true,
-  );
-  const officeRef = useRef();
-  const addressRef = useRef();
-  const emailRef = useRef();
-  const reasonRef = useRef();
+  const [isIntern, setIsIntern] = useState(false);
+  const [isWork, setIsWork] = useState(false);
+  const dateRef = useRef();
+  const introRef = useRef();
 
   useEffect(() => {
     load();
   }, []);
-
-  const load = () => {
-    officeRef.current.value = localStorage.getItem('office');
-    addressRef.current.value = localStorage.getItem('address');
-    emailRef.current.value = localStorage.getItem('eamil');
-    reasonRef.current.value = localStorage.getItem('reason');
-  };
 
   const modal = useModal({
     title: '저장되었습니다.',
     onOk: () => {},
   });
 
+  const load = () => {
+    setIsIntern(localStorage.getItem('isIntern'));
+    setIsWork(localStorage.getItem('isWork'));
+    dateRef.current.value = localStorage.getItem('date');
+    introRef.current.value = localStorage.getItem('intro');
+  };
+
   const save = () => {
-    localStorage.setItem('category', category);
-    localStorage.setItem('isRecruiting', isRecruiting);
-    localStorage.setItem('office', officeRef.current.value);
-    localStorage.setItem('address', addressRef.current.value);
-    localStorage.setItem('email', emailRef.current.value);
-    localStorage.setItem('reason', reasonRef.current.value);
+    localStorage.setItem('isIntern', isIntern);
+    localStorage.setItem('isWork', isWork);
+    localStorage.setItem('date', dateRef.current.value);
+    localStorage.setItem('intro', introRef.current.value);
   };
 
   return (
@@ -65,18 +57,17 @@ const Staff = () => {
             <Button
               style={{
                 width: 80,
-                backgroundColor: !(category === '근로') && theme.colors.grey2,
+                backgroundColor: !isWork && theme.colors.grey2,
               }}
-              onClick={() => setCategory('근로')}
+              onClick={() => setIsWork((prev) => !prev)}
             >
               근로
             </Button>
             <Button
               style={{
-                backgroundColor:
-                  !(category === '직장체험형 인턴') && theme.colors.grey2,
+                backgroundColor: !isIntern && theme.colors.grey2,
               }}
-              onClick={() => setCategory('직장체험형 인턴')}
+              onClick={() => setIsIntern((prev) => !prev)}
             >
               직장체험형 인턴
             </Button>
@@ -84,41 +75,13 @@ const Staff = () => {
         </S.ContentWrapper>
 
         <S.ContentWrapper>
-          <p style={{ fontSize: 20, fontWeight: 600 }}>모집 여부</p>
-          <div style={{ display: 'flex', gap: 10 }}>
-            <Button
-              style={{
-                backgroundColor: !isRecruiting && theme.colors.grey2,
-              }}
-              onClick={() => setIsRecruiting(true)}
-            >
-              모집중
-            </Button>
-            <Button
-              style={{
-                backgroundColor: isRecruiting && theme.colors.grey2,
-              }}
-              onClick={() => setIsRecruiting(false)}
-            >
-              모집 완료
-            </Button>
-          </div>
+          <p style={{ fontSize: 20, fontWeight: 600 }}>마감일</p>
+          <DateInput ref={dateRef} />
         </S.ContentWrapper>
+
         <S.ContentWrapper>
-          <p style={{ fontSize: 20, fontWeight: 600 }}>학과/사무실 이름</p>
-          <Input ref={officeRef} />
-        </S.ContentWrapper>
-        <S.ContentWrapper>
-          <p style={{ fontSize: 20, fontWeight: 600 }}>주소</p>
-          <Input ref={addressRef} />
-        </S.ContentWrapper>
-        <S.ContentWrapper>
-          <p style={{ fontSize: 20, fontWeight: 600 }}>이메일</p>
-          <Input ref={emailRef} />
-        </S.ContentWrapper>
-        <S.ContentWrapper>
-          <p style={{ fontSize: 20, fontWeight: 600 }}>신청사유</p>
-          <TextArea ref={reasonRef} />
+          <p style={{ fontSize: 20, fontWeight: 600 }}>부서소개</p>
+          <TextArea ref={introRef} style={{ height: 400 }} />
         </S.ContentWrapper>
       </div>
       <div style={{ display: 'flex', gap: 10 }}>
@@ -145,7 +108,7 @@ const S = {
     width: 100vw;
     height: calc(100vh - 70px);
     display: flex;
-    justify-content: center;
+    padding-top: 50px;
     flex-direction: column;
     gap: 30px;
     align-items: center;
